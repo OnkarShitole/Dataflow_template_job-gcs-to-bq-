caa1f0b0ff4298e2eb4cab450826ec7869fc75e4
Onkar Shitole <onkarshitole89@gmail.com>
2025-08-30 18:22:58 +0530

    Add files via upload

--- udf.js
+++ udf.js
@@ -2,16 +2,17 @@ function transform(line) {
     var values = line.split(',');
 
     // Check if the line is the header by checking the first value
-    if (values[0].toLowerCase() === 'id') {
+    if (values[0].toLowerCase() === 'sale_id') {
         return null; // Ignore the header row
     }
 
     var obj = new Object();
-    obj.id = values[0];
-    obj.name = values[1];
-    obj.email = values[2];
-    obj.age = values[3];
-    obj.city = values[4];
+    obj.sale_id = values[0];
+    obj.sale_date = values[1];
+    obj.amount = values[2];
+    obj.customer_id = values[3];
     var jsonString = JSON.stringify(obj);
     return jsonString;
 }
+
+			
